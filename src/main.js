9dfedada6414df224abcb46b4b1da7d0ece7bb5c
Nicholas Barnhart <nicholasbarnhart@gmail.com>
2017-11-28 08:08:44 -0800

    added docs folder

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
-import('../node_modules/vuetify/dist/vuetify.min.css')
+import('../node_modules/vuetify/docs/vuetify.min.css')
 import VeeValidate from 'vee-validate'
 import ContactForm from '@/components/ContactForm'
 
